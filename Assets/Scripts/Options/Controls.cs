6261df07310a7810c4862b3b9f1dcd0bc5ac5d65
DESKTOP-5Q35SJU\Sean <seanspicer@hotmail.com>
2019-05-30 14:23:36 +0100

    Getting things ready

---
+++ Assets/Scripts/Options/Controls.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Controls
+{
+    //store keycode value for inputs, if we want to change controls we can do this
+    public ControlBinding forwards, backwards, strafeLeft, strafeRight, rotateLeft, rotateRight, walkRun, jump, autoRun;
+
+
+
+}
